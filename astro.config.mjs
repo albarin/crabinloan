5657a99d30eb219b3301750a214a9057944fd87a
Alba Rincón <alba@rincon.me>
2025-12-16 22:29:46 +0100

    Fix umami

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-import yeskunallumami from '@yeskunall/astro-umami';
+import umami from "@yeskunall/astro-umami";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://albarin.github.io',
-	integrations: [mdx(), sitemap(), yeskunallumami(
+	integrations: [mdx(), sitemap(), umami(
 		{
 			id: 'deeb9f1a-60c4-46fb-ba2d-a0473a6e0347',
 			domains: ['crabinloan.com', 'www.crabinloan.com'],
